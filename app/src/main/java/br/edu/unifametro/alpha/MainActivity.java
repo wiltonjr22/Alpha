65d2742070ec8165559fe8c171d12614a0de4691
Wilton Souza Rodrigues Junior <wiltonsousajr@hotmail.com>
2020-10-08 09:54:01 -0300

    termino do projeto

--- app/src/main/java/br/edu/unifametro/alpha/MainActivity.java
+++ app/src/main/java/br/edu/unifametro/alpha/MainActivity.java
@@ -6,14 +6,30 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText idade01;
+    private TextView textResultdo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        idade01 = findViewById(R.id.idade01);
+        textResultdo= findViewById(R.id.textResultado);
+
     }
+
+    public void calcularIdade(View view) {
+        int idade = Integer.parseInt(idade01.getText().toString());
+        int resultado = idade*7;
+        textResultdo.setText("idade igual a :"+resultado+"anos");
+    }
+
     public void startSegundaActivity(View v) {
       Intent intent = new Intent(this,videoIdade.class);
        startActivity(intent);
