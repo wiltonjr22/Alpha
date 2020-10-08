8dac28b24ab885003ca2be08e1bef0f6eca5cc95
Wilton Souza Rodrigues Junior <wiltonsousajr@hotmail.com>
2020-10-07 22:55:04 -0300

    teste 03

--- app/src/main/java/br/edu/unifametro/alpha/MainActivity.java
+++ app/src/main/java/br/edu/unifametro/alpha/MainActivity.java
@@ -12,13 +12,5 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-    }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-    }
 }
