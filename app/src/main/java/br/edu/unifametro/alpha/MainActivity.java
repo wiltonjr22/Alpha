946f561aa292e10bb954e45dcebb3f76a002b139
Wilton Souza Rodrigues Junior <wiltonsousajr@hotmail.com>
2020-11-17 17:08:13 -0300

    Persistencia de dado
    implementaçao da funcionalidade

--- app/src/main/java/br/edu/unifametro/alpha/MainActivity.java
+++ app/src/main/java/br/edu/unifametro/alpha/MainActivity.java
@@ -3,16 +3,26 @@ package br.edu.unifametro.alpha;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText idade01;
     private TextView textResultdo;
+
+
+    public static final String PREFS_NAME = "prefs";
+    private Button btnOnOff;
+
+    SharedPreferences settings = getSharedPreferences(PREFS_NAME,0);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +32,20 @@ public class MainActivity extends AppCompatActivity {
         idade01 = findViewById(R.id.idade01);
         textResultdo= findViewById(R.id.textResultado);
 
+        btnOnOff = (Button) findViewById(R.id.btn);
+
+        btnOnOff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences settings = getSharedPreferences(PREFS_NAME,0);
+                SharedPreferences.Editor editor = settings.edit();
+                editor.putStringSet("resultado", );
+                editor.commit();
+            }
+        });
+
     }
+
 
     public void calcularIdade(View view) {
         int idade = Integer.parseInt(idade01.getText().toString());
